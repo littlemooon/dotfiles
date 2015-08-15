05cc9b0c9d6dc94a87d66230b099a540f52bbe14
fred wright <fredwright0@gmail.com>
2015-08-15 13:58:36 +0200

    Add cask and install apps

--- install.sh
+++ install.sh
@@ -6,9 +6,36 @@ brew update
 brew doctor # if this fails run: export PATH="/usr/local/bin:$PATH"
 
 echo '--- installing packages'
-brew install node
+brew install nvm
+nvm install stable
 brew install git
 brew install tmux
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+brew cask install alfred
+brew cask install keepingyouawake
+brew cask install google-chrome
+brew cask install firefoxdeveloperedition
+brew cask install bettertouchtool
+brew cask install flux
+brew cask install bartender
+brew cask install dash
+brew cask install spotify
+brew cask install gfxcardstatus
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install slack
+brew cask install boom
+brew cask install sublime-text
+brew cask install skype
+brew cask install lastpass
+brew cask install vlc
+brew tap caskroom/fonts
+brew cask install font-inconsolata
+brew cask install font-fira-sans
+brew cask install font-fira-code
+brew cask install font-droid-sans
+brew cask install font-droid-sans-mono
 
 echo '--- downloading vundle'
 git clone https://github.com/gmarik/Vundle.vim.git $DIR/vim/bundle/Vundle.vim
@@ -18,9 +45,6 @@ git config --global credential.helper osxkeychain
 
 echo '--- downloading antigen'
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > $DIR/zsh/antigen.zsh
-
-echo '--- downloading fira code font'
-curl -L https://github.com/tonsky/FiraCode/releases/download/0.3/FiraCode-Regular.otf > $DIR/FiraCode-Regular.otf
 
 echo '--- setting zshenv'
 cat <<EOM > $HOME/.zshenv
