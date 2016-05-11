c6faee8742d9447e78bcbc7daf9dbe4e751db315
fred wright <fredwright0@gmail.com>
2016-05-11 11:59:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,21 +49,21 @@ brew cask install keepingyouawake
 brew cask install google-chrome
 brew cask install chromecast
 brew cask install tunnelblick
-brew cask install firefoxdeveloperedition
 brew cask install bettertouchtool
 brew cask install flux
 brew cask install dash
 brew cask install spotify
 brew cask install virtualbox
 brew cask install vagrant
+brew cask install vagrant-manager
 brew cask install slack
-brew cask install sublime-text3
+brew cask install atom
 brew cask install skype
 brew cask install java
 brew cask install vlc
 brew cask install github-desktop
 brew cask install transmission
-brew cask install goofy
+brew cask install franz
 brew cask install sequel-pro
 brew tap caskroom/fonts
 brew cask install font-inconsolata
