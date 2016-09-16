0a5a3226152c8899d16eb04b565696e8ad6682d7
fred wright <fredwright0@gmail.com>
2016-09-16 11:04:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -67,3 +67,24 @@ brew cask install font-fira-code
 brew cask install font-droid-sans
 brew cask install font-droid-sans-mono
 brew cask cleanup
+
+apm install atom-beautify
+apm install atom-prettify
+apm install auto-detect-indentation
+apm install autoclose-html
+apm install dracula-theme
+apm install file-icons
+apm install highlight-selected
+apm install language-babel
+apm install language-ejs
+apm install lines
+apm install linter
+apm install linter-clojure
+apm install linter-eslint
+apm install linter-rubocop
+apm install oceanic-next
+apm install pretty-json
+apm install project-manager
+apm install react
+apm install react-snippets
+apm install spacegray-dark-neue-ui
