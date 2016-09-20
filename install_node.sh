28276643ba1643241128052b37a5a6ecc47ed5ff
fred wright <fredwright0@gmail.com>
2016-09-20 14:51:50 +0200

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -4,4 +4,4 @@ export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
 nvm install stable
 nvm alias default stable
-npm i eslint eslint-config-standard eslint-config-standard-react eslint-import-resolver-webpack eslint-plugin-babel eslint-plugin-import eslint-plugin-promise eslint-plugin-react eslint-plugin-standard babel-eslint serverless npm-check-updates -g
+npm i eslint eslint-config-standard eslint-config-standard-react eslint-import-resolver-webpack eslint-plugin-babel eslint-plugin-import eslint-plugin-promise eslint-plugin-react eslint-plugin-standard babel-eslint npm-check-updates -g
