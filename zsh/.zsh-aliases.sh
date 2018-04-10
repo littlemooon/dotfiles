af29257e7778162ccc468fb98d5aad29bd4fc39a
fred wright <fredwright0@gmail.com>
2018-04-10 20:46:45 +0800

    🎉 update aliases

--- zsh/.zsh-aliases.sh
+++ zsh/.zsh-aliases.sh
@@ -1,38 +1,53 @@
 #aliases
 
 alias .='cd ..'
-alias zshrc='vim ~/.zshrc'
+alias zshrc='vim ~/.zsh-aliases'
 alias rezsh='source ~/.zshrc'
 alias cl='clear'
 alias mux='tmuxinator'
+alias wifi='wifi-password'
+alias rn='react-native'
+alias cleanbranches='git branch | grep -v "master" | xargs git branch -D'
 
 # git helpers
 
 zc () { git checkout $1; }
-zb () { git checkout -b $1 -t develop; }
+zb () { git checkout -b $1 -t master; }
 zm () { git commit -m $*; }
-zp () { git push -u origin $(current_branch); }
+zp () { git push --follow-tags -u origin $(current_branch); }
 
 alias zs='git status'
-alias za='git add --all .'
-alias zu='cl && git pull --rebase origin develop'
+alias za='git add --all . && git reset .env'
+alias zu='cl && git pull --rebase origin'
+alias zum='cl && git pull --rebase origin master'
 alias zup='git stash && zu && git stash pop'
 alias zl='cl && git log --graph --decorate --all'
 alias zf='git fetch'
 alias zd='git diff'
 alias zcd='git checkout develop'
+alias zcm='git checkout master'
 
 # project helpers
+
+alias ni='cl && npm i'
+alias nuke='cl && sudo rm -rf node_modules && sudo yarn cache clean && yarn'
+alias nc='npm-check -u'
+alias ns='cl && npm start'
+alias nt='cl && npm test'
+alias ntw='cl && npm run test:watch'
+alias nf='cl && npm run flow'
+alias nfb='cl && npm run flow:build'
+alias nd='cl && npm run dev'
+alias nb='cl && npm run build'
+alias nl='cl && npm run lint'
+alias nr='cl && npm run'
+
+alias j='cl && jest'
+alias jw='cl && jest -w'
 
 alias vr='cl && vagrant resume'
 alias vu='cl && vagrant up'
 alias vh='cl && vagrant halt'
-
-alias ni='cl && yarn'
-alias nc='cl && sudo rm -rf node_modules && sudo npm cache clean && npm i'
-alias ns='cl && yarn start'
-alias nt='cl && yarn test'
-alias nl='cl && yarn run lint'
 
 # process helpers
 
