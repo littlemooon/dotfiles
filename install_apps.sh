ed04df1812cf7352619ce9c29cc5239f9f93ee52
fred wright <fredwright0@gmail.com>
2025-01-21 15:01:20 +0800

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,21 +1,9 @@
 echo '--- apps'
 
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
-
-brew cask install 1password
-brew cask install bettertouchtool
-brew cask install dropbox
-brew cask install flux
-brew cask install franz
-brew cask install github-desktop
-brew cask install google-chrome
-brew cask install spotify
-brew cask install totalspaces
-brew cask install visual-studio-code
-brew cask install vlc
-
-brew tap caskroom/fonts
-brew cask install font-fira-code
-
-brew cask cleanup
+brew install --cask 1password
+brew install --cask font-fira-code
+brew install --cask firefox
+brew install --cask ghostty
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask vlc
